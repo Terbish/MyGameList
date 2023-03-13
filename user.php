f2d647b1f60d7e8d56db98991d2e55c3c0e8bf95
Jerry Shen <terb.avg@gmail.com>
2023-03-13 10:41:57 -0700

    made some style changes to login.html

--- user.php
+++ user.php
@@ -9,6 +9,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Page</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            max-width: 800px;
+            margin: 0 auto;
+        }
+
+        th, td {
+            text-align: left;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #0077cc;
+            color: white;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        tr:hover {
+            background-color: #ddd;
+        }
+        </style>
 </head>
 <body>
     <div>
